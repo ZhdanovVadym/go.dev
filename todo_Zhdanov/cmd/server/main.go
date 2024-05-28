b7b55a1fec63d6aa37e0c6906837bf51d3661dd0
ZhdanovVadym <159918865+ZhdanovVadym@users.noreply.github.com>
2024-05-28 12:23:28 +0300

    Add files via upload

---
+++ todo_Zhdanov/cmd/server/main.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"runtime/debug"
+	"strconv"
+	"syscall"
+
+	"github.com/BohdanBoriak/boilerplate-go-back/config"
+	"github.com/BohdanBoriak/boilerplate-go-back/config/container"
+	"github.com/BohdanBoriak/boilerplate-go-back/internal/infra/database"
+	"github.com/BohdanBoriak/boilerplate-go-back/internal/infra/http"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	exitCode := 0
+	ctx, cancel := context.WithCancel(context.Background())
+
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Printf("The system panicked!: %v\n", r)
+			fmt.Printf("Stack trace form panic: %s\n", string(debug.Stack()))
+			exitCode = 1
+		}
+		os.Exit(exitCode)
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		sig := <-c
+		fmt.Printf("Received signal '%s', stopping... \n", sig.String())
+		cancel()
+		fmt.Printf("Sent cancel to all threads...")
+
+		deleteResources()
+	}()
+
+	var conf = config.GetConfiguration()
+
+	err := database.Migrate(conf)
+	if err != nil {
+		log.Fatalf("Unable to apply migrations: %q\n", err)
+	}
+
+	cont := container.New(conf)
+
+
+	err = http.Server(
+		ctx,
+		setupRouter(cont),
+	)
+	if err != nil {
+		fmt.Printf("http server error: %s", err)
+		exitCode = 2
+		return
+	}
+}
+
+
+func deleteResources() {
+	fmt.Println("Deleting resources...")
+
+}
+
+func setupRouter(cont *container.Container) *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/tasks", createTask).Methods("POST")
+	r.HandleFunc("/tasks/{id}", getTask).Methods("GET")
+	r.HandleFunc("/tasks/{id}", updateTask).Methods("PUT")
+	r.HandleFunc("/tasks/{id}", deleteTask).Methods("DELETE")
+	return r
+}
+
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+	var task Task
+	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := db.Insert(&task); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(task)
+}
+
+func getTask(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid task ID", http.StatusBadRequest)
+		return
+	}
+	var task Task
+
+	if err := db.Get(&task, id); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(task)
+}
+
+func updateTask(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid task ID", http.StatusBadRequest)
+		return
+	}
+	var task Task
+	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	task.ID = id
+
+	if err := db.Update(&task); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(task)
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid task ID", http.StatusBadRequest)
+		return
+	}
+
+	if err := db.Delete(id); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
